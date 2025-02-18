8b435393badf90ddfdf2150a399c7f6372d360f2
Your Name <alessanrospedroso@outlook.com>
2025-02-18 17:53:59 -0300

    primeiro commit do projeto

---
+++ src/pages/detail/index.tsx
@@ -0,0 +1,7 @@
+export function Detail() {
+    return (
+        <div>
+            <h1>Detalhe da moeda</h1>
+        </div>
+    )
+}
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./home.module.css"
+
+export function Home() {
+    return (
+        <div>
+            <h1>Página Home</h1>
+        </div>
+    )
+}
